22f70519283ba50beaf419f068494014ee50c620
Douglas Gonçalves <douglassg86@gmail.com>
2025-11-14 11:47:05 +0000

    o logo que se apresenta no canto superior esquerdo deve ser o logo_grupo

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -1,4 +1,4 @@
-import { PlaengeVanguardLogo } from "@/components/icons";
+import Image from "next/image";
 
 export function SiteFooter() {
   const currentYear = new Date().getFullYear();
@@ -6,7 +6,13 @@ export function SiteFooter() {
     <footer className="border-t">
       <div className="container flex flex-col items-center justify-center gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <PlaengeVanguardLogo className="fill-current text-primary" />
+          <Image
+            src="/INSTITUCIONAL/logo_grupo_plaenge.png"
+            alt="Grupo Plaenge Logo"
+            width={150}
+            height={40}
+            className="h-auto"
+          />
           <p className="text-center text-sm leading-loose md:text-left text-muted-foreground">
             © {currentYear} Grupo Plaenge. Todos os direitos reservados.
           </p>
