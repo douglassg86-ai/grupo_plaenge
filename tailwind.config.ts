58557f539d5f782315802348b94f222b78f3bee9
Douglas Gonçalves <douglassg86@gmail.com>
2025-11-16 15:35:51 +0000

    undo my two last actions on terminal

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ["'Montserrat'", 'sans-serif'],
-        headline: ["'Montserrat'", 'serif'],
+        body: ["'PT Sans'", 'sans-serif'],
+        headline: ["'Playfair Display'", 'serif'],
         code: ['monospace'],
       },
       colors: {
