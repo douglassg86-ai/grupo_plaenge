a9d3c77b568c3bcda6b12353c0a12606a5414371
Douglas Gonçalves <douglassg86@gmail.com>
2025-12-27 13:26:47 +0000

    na página do empreendimento WAVE, retirar o link que remete a página ini

--- src/components/wave/header.tsx
+++ src/components/wave/header.tsx
@@ -5,9 +5,7 @@ export default function Header() {
   return (
     <header className="py-4 absolute top-0 left-0 w-full z-10">
       <div className="container flex justify-center items-center">
-        <Link href="/">
-            <Image src="/INSTITUCIONAL/logo_grupo_plaenge.png" alt="Grupo Plaenge Logo" width={150} height={40} data-ai-hint="company logo" />
-        </Link>
+        <Image src="/INSTITUCIONAL/logo_grupo_plaenge.png" alt="Grupo Plaenge Logo" width={150} height={40} data-ai-hint="company logo" />
       </div>
     </header>
   );
