2515c7080ba91878dc913c6cc0f2955c281f0554
Douglas Gonçalves <douglassg86@gmail.com>
2026-01-13 20:48:54 +0000

    o erro persiste, a página não esta mais carregando fora do ambiente do f

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { cn } from '@/lib/utils';
 import { Toaster } from '@/components/ui/toaster';
-import { FirebaseClientProvider } from '@/firebase/client-provider';
 
 export const metadata: Metadata = {
   title: 'Plaenge & Vanguard Showcase',
@@ -29,9 +28,7 @@ export default function RootLayout({
           'min-h-screen font-body antialiased'
         )}
       >
-        <FirebaseClientProvider>
-          {children}
-        </FirebaseClientProvider>
+        {children}
         <Toaster />
       </body>
     </html>
